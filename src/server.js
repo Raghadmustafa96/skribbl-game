44744b78a5965caa6d115f8331f561893f46ab44
zaidalasfar97 <z.alasfar@hotmail.com>
2021-04-13 22:37:25 +0300

    not finish

--- src/server.js
+++ src/server.js
@@ -15,6 +15,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
+
 module.exports = {
   server,
   app,
