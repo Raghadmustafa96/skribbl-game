ae552e8c620e514347848300215b1456480b0264
Omar Hakem <youtarawneh997@gmail.com>
2021-04-13 12:24:38 +0300

    setting up the files

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(http);
+require('dotenv').config();
 
 const { v4: uuidv4 } = require('uuid');
-
-
 const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(cors());
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 server.listen(PORT, () => console.log('Listening on PORT ' + PORT));
