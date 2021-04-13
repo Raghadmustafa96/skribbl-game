1afaac8fd5ebe0b50fc64c12dab21d06e9c51a2d
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-04-14 00:22:16 +0300

    add to ejs

--- src/server.js
+++ src/server.js
@@ -1,68 +1,63 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(http);
-const { v4: uuidv4 } = require('uuid');
+const io = require("socket.io")(http);
+const { v4: uuidv4 } = require("uuid");
 io.listen(server);
 
-// const link = require('../public/js/app.js')
-
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(cors());
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 let currentRoom;
 const queue = {
   tickets: [],
   staff: [],
 };
+let link = uuidv4();
+app.get("/", (req, res) => {
+  res.render("index", { data: link });
+});
 
+app.get("/" + link, (req, res) => {
+  res.render("admin");
+});
 
-// app.get('/', (req, res) => {
-//   res.render('index');
-// });
-
-// app.get('/:id', (req, res) => {
-//   res.render('index');
-// });
-
-io.on('connection', (socket) => {
-  console.log('a user Connected', socket.id);
-  socket.on('join', (payload) => {
+io.on("connection", (socket) => {
+  console.log("a user Connected", socket.id);
+  socket.on("join", (payload) => {
     currentRoom = payload.room;
     socket.join(currentRoom);
     console.log(currentRoom);
-    const data = {id: socket.id };
+    const data = { id: socket.id };
     queue.staff.push(data);
   });
 
-  socket.on('createTicket', (payload) => {
+  socket.on("createTicket", (payload) => {
     const data = { id: uuidv4(), ...payload };
     queue.tickets.push(data);
-    socket.in(currentRoom).emit('newTicket', { ...data, socketId: socket.id }); // 3s
+    socket.in(currentRoom).emit("newTicket", { ...data, socketId: socket.id }); // 3s
   });
 
-  socket.on('getall', () => {
+  socket.on("getall", () => {
     queue.staff.forEach((staff) => {
-      socket.emit('', { name: staff.name });
+      socket.emit("", { name: staff.name });
     });
     queue.tickets.forEach((ticket) => {
-      socket.emit('newTicket', ticket);
+      socket.emit("newTicket", ticket);
     });
   });
-  socket.on('disconnect', () => {
-    socket.to(currentRoom).emit('offlineStaff', { id: socket.id });
+  socket.on("disconnect", () => {
+    socket.to(currentRoom).emit("offlineStaff", { id: socket.id });
     queue.staff = queue.staff.filter((s) => s.id !== socket.id);
   });
 });
 
 // console.log(link);
-
 
 module.exports = {
   server,
