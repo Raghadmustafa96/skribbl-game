309a08baa89445dde95e14951f3f46866e159779
Omar Hakem <youtarawneh997@gmail.com>
2021-04-15 15:48:55 +0300

    adding the rooms

--- index.js
+++ index.js
@@ -6,16 +6,51 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users');
 
+const skribbleBot = 'Skribble Bot';
 const PORT = 3000 || process.env.PORT;
 // set Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 function onConnection(socket) {
-  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  socket.on('drawing', (data) => {
+    socket.emit('drawing', data);
+  });
 }
 
-io.on('connection', onConnection);
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+    // welcome current player
+    onConnection(socket);
+    socket.emit(
+      'message',
+      formatMessage(skribbleBot, 'Welcome to Skribble Game')
+    );
+    // broadcast to other playes that someone join
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(skribbleBot, `${user.username} has joined the game`)
+      );
+  });
+  onConnection(socket);
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+});
 server.listen(PORT, () => {
   console.log('server is running or PORT: ', PORT);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,117 +1,46 @@
 'use strict';
 
-(function () {
-  var socket = io();
-  var canvas = document.getElementsByClassName('whiteboard')[0];
-  var colors = document.getElementsByClassName('color');
-  var context = canvas.getContext('2d');
+const socket = io();
+const chatForm = document.getElementById('chatForm');
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
-  var current = {
-    color: 'black',
-  };
-  var drawing = false;
+// Join the Game
+socket.emit('joinRoom', { username, room });
+socket.on('message', (message) => {
+  console.log('__msg__', message);
+  outputMessage(message);
+});
 
-  canvas.addEventListener('mousedown', onMouseDown, false);
-  canvas.addEventListener('mouseup', onMouseUp, false);
-  canvas.addEventListener('mouseout', onMouseUp, false);
-  canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+// functoins
+function outputMessage(message) {
+  const div = document.createElement('div');
+  // you can add class here for the div
+  const p = document.createElement('p');
+  p.innerText = message.username;
+  p.innerHTML += `<span>${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.innerText = message.text;
+  div.appendChild(para);
+  document.querySelector('.txtArea').appendChild(div);
+}
 
-  for (var i = 0; i < colors.length; i++) {
-    colors[i].addEventListener('click', onColorUpdate, false);
+// events
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // Get Guessed word
+  let msg = e.target.elements.msg.value;
+  msg = msg.trim();
+  if (!msg) {
+    return false;
   }
+  console.log('__chatmessage___', msg);
+  //Emit the msg to server
+  socket.emit('chatMessage', msg);
 
-  socket.on('drawing', onDrawingEvent);
-
-  window.addEventListener('resize', onResize, false);
-  onResize();
-
-  function drawLine(x0, y0, x1, y1, color, emit) {
-    context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
-    context.strokeStyle = color;
-    context.lineWidth = 2;
-    context.stroke();
-    context.closePath();
-
-    if (!emit) {
-      return;
-    }
-    var w = canvas.width;
-    var h = canvas.height;
-
-    socket.emit('drawing', {
-      x0: x0 / w,
-      y0: y0 / h,
-      x1: x1 / w,
-      y1: y1 / h,
-      color: color,
-    });
-  }
-
-  function onMouseDown(e) {
-    drawing = true;
-    current.x = e.clientX || e.touches[0].clientX;
-    current.y = e.clientY || e.touches[0].clientY;
-  }
-
-  function onMouseUp(e) {
-    if (!drawing) {
-      return;
-    }
-    drawing = false;
-    drawLine(
-      current.x,
-      current.y,
-      e.clientX || e.touches[0].clientX,
-      e.clientY || e.touches[0].clientY,
-      current.color,
-      true
-    );
-  }
-
-  function onMouseMove(e) {
-    if (!drawing) {
-      return;
-    }
-    drawLine(
-      current.x,
-      current.y,
-      e.clientX || e.touches[0].clientX,
-      e.clientY || e.touches[0].clientY,
-      current.color,
-      true
-    );
-    current.x = e.clientX || e.touches[0].clientX;
-    current.y = e.clientY || e.touches[0].clientY;
-  }
-
-  function onColorUpdate(e) {
-    current.color = e.target.className.split(' ')[1];
-  }
-
-  // limit the number of events per second
-  function throttle(callback, delay) {
-    var previousCall = new Date().getTime();
-    return function () {
-      var time = new Date().getTime();
-
-      if (time - previousCall >= delay) {
-        previousCall = time;
-        callback.apply(null, arguments);
-      }
-    };
-  }
-
-  function onDrawingEvent(data) {
-    var w = canvas.width;
-    var h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
-  }
-
-  // make the canvas fill its parent
-  function onResize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
-})();
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
