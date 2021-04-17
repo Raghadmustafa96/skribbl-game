15658020b290564d585ce6ca9f262959fefd2904
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-04-17 17:13:15 +0300

    looping words and styling

--- index.js
+++ index.js
@@ -1,153 +1,171 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const path = require('path');
-const http = require('http');
+const express = require("express");
+const path = require("path");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server);
-const formatMessage = require('./utils/messages');
+const io = require("socket.io")(server);
+const formatMessage = require("./utils/messages");
 const {
   userJoin,
   getCurrentUser,
   userLeave,
   getRoomUsers,
-} = require('./utils/users');
+} = require("./utils/users");
 
+const words = ["test", "money"];
 const queue = {
   staff: [],
 };
-
+let trn = 0;
+let allPlayers = 0;
 let players = [];
 let current_turn = 0;
 let timeOut;
 let _turn = 0;
 const MAX_WAITING = 10000;
 
-const skribbleBot = 'Skribble Bot';
-const PORT = 3030 || process.env.PORT;
+const skribbleBot = "Skribble Bot";
+const PORT = 3000 || process.env.PORT;
 // set Static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 function onConnection(socket, room) {
-  socket.on('drawing', (data) => {
-    socket.broadcast.to(room).emit('drawing', data);
+  socket.on("drawing", (data) => {
+    socket.broadcast.to(room).emit("drawing", data);
   });
 }
 
-io.on('connection', (socket) => {
-  console.log('A player connected');
+io.on("connection", (socket) => {
+  // console.log("A player connected", socket.id);
 
-  players.push(socket);
-
-  socket.on('pass_turn',function(){
-    console.log('........................................................' );
-    if(players[_turn] == socket){
-      console.log('........................................................ turn ');
-       resetTimeOut();
-       next_turn();
-    }
-  })
-
-
-  socket.on('joinRoom', ({ username, room }) => {
+  socket.on("joinRoom", ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
     socket.join(user.room);
-
-    const data = { name: username,room: user.room, id: socket.id };
+    allPlayers++;
+    const data = {
+      name: username,
+      room: user.room,
+      id: socket.id,
+      points: user.points,
+    };
     queue.staff.push(data);
+    players.push(socket);
+    console.log(players[0].id, "============players========");
 
     onConnection(socket);
     // welcome current player
     socket.emit(
-      'message',
-      formatMessage(skribbleBot, 'Welcome to Skribble Game')
+      "message",
+      formatMessage(skribbleBot, "Welcome to Skribble Game")
     );
     // broadcast to other playes that someone join
     socket.broadcast
       .to(user.room)
       .emit(
-        'message',
+        "message",
         formatMessage(skribbleBot, `${user.username} has joined the game`)
       );
     // send players info
-    socket.to(user.room).emit('roomPlayers', data);
+    socket.to(user.room).emit("roomPlayers", data);
     // socket.to(user.room).emit('room', data);
 
-    socket.on('getall', () => {
-      console.log(queue);
+    socket.on("getall", () => {
       queue.staff.forEach((staff) => {
-        socket.emit('roomPlayers', {name : staff.name , room : staff.room , id : staff.id});
+        socket.emit("roomPlayers", {
+          name: staff.name,
+          room: staff.room,
+          id: staff.id,
+          points: staff.points,
+        });
       });
     });
-
+    socket.on("pass_turn", function () {
+      if (players[trn] == socket) {
+        players[trn].emit("showword", "test");
+        resetTimeOut();
+        next_turn();
+      }
+    });
   });
 
-  onConnection(socket, 'room1');
-  socket.on('chatMessage', (msg) => {
+  onConnection(socket, "room1");
+  socket.on("chatMessage", (msg) => {
     const user = getCurrentUser(socket.id);
-
-    socket.to(user.room).emit('message', formatMessage(user.username, msg));
+    if (msg === words[0]) {
+      console.log(user.points);
+      io.emit("points", user.points);
+      socket
+        .to(user.room)
+        .emit(
+          "message",
+          formatMessage(user.username, user.username + " Found the word")
+        );
+    } else {
+      socket.to(user.room).emit("message", formatMessage(user.username, msg));
+    }
   });
 
-  socket.on('disconnect', () => {
-    socket.to('room1').emit('offlineStaff', { id: socket.id });
+  socket.on("disconnect", () => {
+    socket.to("room1").emit("offlineStaff", { id: socket.id });
 
     const user = userLeave(socket.id);
 
     queue.staff = queue.staff.filter((s) => s.id !== socket.id);
-    console.log(' queue.staff' ,  queue.staff );
 
-
-    console.log('A player disconnected');
-    players.splice(players.indexOf(socket),1);
-    _turn--;
-    console.log("A number of players now ",players.length);
-
+    console.log("A player disconnected");
+    players.splice(players.indexOf(socket), 1);
+    allPlayers = queue.staff.length;
     if (user) {
       io.to(user.room).emit(
-        'message',
+        "message",
         formatMessage(skribbleBot, `${user.username} has left the Game`)
       );
     }
   });
 });
 
+function next_turn() {
+  // _turn = current_turn++ % players.length;
 
-function next_turn(){
-  console.log('........................................................ * ');
-
-   _turn = current_turn++ % players.length;
-   players[_turn].emit('start turn','A player connected');
-   console.log('........................................................ ** ');
-
-   console.log("next turn triggered " , _turn);
-   triggerTimeout();
-   players[_turn].emit('end turn','A player connected');
-
-   console.log('........................................................ *** ');
-
+  // console.log(players[trn], "========error========");
+  players[trn].emit("start turn", "A player connected");
+  triggerTimeout();
+  console.log(allPlayers, "=============", trn, players.length);
+  // if (players[trn] == undefined) {
+  //   trn = 0;
+  // }
+  trn++;
+  console.log(allPlayers, "=============", trn);
+  console.log(allPlayers, "=============", trn);
 }
 
-
-function triggerTimeout(){
+function triggerTimeout() {
   _turn = current_turn++ % players.length;
-  console.log('........................................................ triger ');
 
-  timeOut = setTimeout(()=>{
+  timeOut = setTimeout(() => {
+    console.log("=====trn====", trn);
+    if (trn >= players.length || players[trn] == undefined) {
+      console.log("=====trn==in==", trn);
+      players[trn - 1].emit("end turn", "A player connected");
+      trn = 0;
+      console.log("=====trn=out===", trn);
+      console.log("==Next round==");
+    } else {
+      players[trn - 1].emit("end turn", "A player connected");
+    }
     next_turn();
-  },MAX_WAITING);
+  }, MAX_WAITING);
 }
 
-
-function resetTimeOut(){
-   if(typeof timeOut === 'object'){
-     console.log("timeout reset");
-     clearTimeout(timeOut);
-   }
+function resetTimeOut() {
+  if (typeof timeOut === "object") {
+    console.log("timeout reset");
+    clearTimeout(timeOut);
+  }
 }
-
 
 server.listen(PORT, () => {
-  console.log('server is running or PORT: ', PORT);
+  console.log("server is running or PORT: ", PORT);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,103 +1,110 @@
-'use strict';
+"use strict";
 
 const options = {
-  transports: ['websocket'],
+  transports: ["websocket"],
 };
-const socket = io('localhost:3030/', options);
-const chatForm = document.getElementById('chatForm');
+const socket = io("localhost:3000/", options);
+const chatForm = document.getElementById("chatForm");
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
 // Join the Game
-socket.emit('joinRoom', { username, room });
+socket.emit("joinRoom", { username, room });
 
-socket.emit('getall');
+socket.emit("getall");
 
-socket.on('message', (message) => {
-  console.log('__msg__', message);
+socket.on("showword", (data) => {
+  console.log(data, "the new word =========================");
+  $("#text").show();
+});
+
+socket.on("message", (message) => {
+  console.log("__msg__", message);
   outputMessage(message);
 });
 
-socket.on('room', (data) => {
+socket.on("room", (data) => {
   outputRoomName(data.room);
 });
 
-
-$("#turn").on("click",function(e){
-  socket.emit('pass_turn');
-})
-
-socket.on('start turn', (data) => {
-console.log(data , 'start turn')
-$('#word').removeAttr('hidden');
-
+$("#turn").on("click", (e) => {
+  socket.emit("pass_turn");
 });
 
-
-socket.on('end turn', (data) => {
-console.log(data , 'end turn')
-$('#word').toggle();
+socket.on("start turn", (data) => {
+  console.log(data, "start turn");
+  $(".word").fadeIn();
 });
 
-
+socket.on("end turn", (data) => {
+  console.log(data, "end turn hide the word");
+  $(".word").fadeOut();
+});
 
 // get Room and players
-socket.on('roomPlayers', (data) => {
-  console.log(data , '__data');
+socket.on("roomPlayers", (data) => {
+  console.log(data, "__data___________");
   output(data);
 });
 
-
-socket.on('offlineStaff', (payload) => {
+socket.on("offlineStaff", (payload) => {
   const el = document.getElementById(payload.id);
-  el.remove();
+  if (el) {
+    console.log(el, "ellllllllllllllllllll");
+    el.remove();
+  }
+});
+socket.on("points", (points) => {
+  console.log(points);
 });
 
 // functoins
 function outputMessage(message) {
-  const div = document.createElement('div');
+  const div = document.createElement("div");
   // you can add class here for the div
-  const p = document.createElement('p');
+  const p = document.createElement("p");
   p.innerText = message.username;
   p.innerHTML += `<span>${message.time}</span>`;
   div.appendChild(p);
-  const para = document.createElement('p');
+  const para = document.createElement("p");
   para.innerText = message.text;
   div.appendChild(para);
-  document.querySelector('.txtArea').appendChild(div);
+  document.querySelector(".txtArea").appendChild(div);
 }
 
 function outputRoomName(room) {
-  let roomDiv = document.getElementById('rooms');
-  let h4 = document.createElement('h4');
+  let roomDiv = document.getElementById("rooms");
+  let h4 = document.createElement("h4");
   h4.innerText = room;
   roomDiv.appendChild(h4);
 }
 
 function output(user) {
-  let ul = document.getElementById('players');
-    let li = document.createElement('li');
-    li.id = user.id;
-    li.innerText = user.name;
-    console.log('__Nmae', user.name);
-    ul.appendChild(li);
+  let ul = document.getElementById("players");
+  let li = document.createElement("li");
+  li.id = user.id;
+  li.innerText = user.name + "  " + user.points + " Points";
+  console.log("__Nmae", user.name);
+  ul.appendChild(li);
 }
 
 // events
-chatForm.addEventListener('submit', (e) => {
+chatForm.addEventListener("submit", (e) => {
+  console.log("start the game");
   e.preventDefault();
   // Get Guessed word
   let msg = e.target.elements.msg.value;
   msg = msg.trim();
+
   if (!msg) {
     return false;
   }
-  console.log('__chatmessage___', msg);
+  console.log("__chatmessage___", msg);
   //Emit the msg to server
-  socket.emit('chatMessage', msg);
+  socket.emit("chatMessage", msg);
 
   // Clear input
-  e.target.elements.msg.value = '';
+  e.target.elements.msg.value = "";
   e.target.elements.msg.focus();
 });
